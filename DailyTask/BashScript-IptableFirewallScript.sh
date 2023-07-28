e00e41d4aa426a6f146710c2e2e1dd9b60f21c03
AlexGu0582 <89766065+Runfirst@users.noreply.github.com>
2023-07-28 16:18:00 +1000

    Update BashScript-IptableFirewallScript.sh

--- DailyTask/BashScript-IptableFirewallScript.sh
+++ DailyTask/BashScript-IptableFirewallScript.sh
@@ -24,4 +24,76 @@ $SYSCTL net.ipv4.icmp_echo_ignore_broadcasts=1
 $SYSCTL net.ipv4.icmp_echo_ignore_bogus_error_messages=1
 $SYSCTL net.ipv4.tcp_syncookies=1
 $SYSCTL net.ipv4.conf.all.rp_filter=1
+$SYSCTL net.ipv4.conf.default.rp_filter=1
+$SYSCTL kernel.exec-shield=1
+$SYSCTL kernel.randomize_va_space=1
+
+
+echo "Starting IPv4 Firewall..."
+$IPT -F 
+$IPT -X 
+$IPT -t nat -F 
+$IPT -t nat -X
+$IPT -t mangle -F 
+$IPT -t mangle -X
+
+
+#Load modules
+[ -f "$BLOCKEDIPS" ] && BADIPS=$(egrep -v -E "^#|^$" "${BLOCKEDIPS}")
+
+
+#interface connected to the Internet
+PUB_IF="eth0"
+
+
+#Unlimited traffic for loopback
+$IPT -A INPUT -i lo -j ACCEPT 
+$IPT -A OUTPUT -l lo -j ACCEPT 
+
+
+#DROP all incoming traffic
+$IPT -P INPUT DROP
+$IPT -P OUTPUT DROP
+$IPT -P FORWARD DROP 
+
+if [ -f "${BLOCKEDIPS}" ]; then
+    #create a new iptables list
+    $IPT -N $SPAMLIST 
+
+    for ipblock in $BADIPS 
+    do 
+        $IPT -A $SPAMLIST -s $ipblock -j LOG --log-profix "$SAPMDROPMSG"
+        $IPT -A $SPAMLIST -s $ipblock -j DROP
+    done 
+
+    $IPT -I INPUT -j $SPAMLIST
+    $IPT -I OUTPUT -j $SPAMLIST
+    $IPT -I FORWARD -j $SPAMLIST 
+fi 
+
+#Block sync
+$IPT -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Drop Sync"
+$IPT -a INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW -j DROP 
+
+
+#Block Fragments
+$IPT -A INPUT -i ${PUB_IF} -f -m limit --limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fragments Packets"
+$IPT -A INPUT -i ${PUB_IF} -f -j DROP 
+
+
+#Block bad stuff
+$IPT -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP 
+$IPT -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL ALL -j DROP 
+
+$IPT -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "NULL Packets"
+$IPT -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -j DROP #NULL packets
+
+$IPT -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+
+$IPT -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "XMAS Packets"
+
+
+
+
+
 
